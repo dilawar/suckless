7899a6b2f9034b39310cd3e3f07bc4f2c33bf78a
Dilawar <dilawars@iitb.ac.in>
2014-03-25 16:13:38 +0530

    install with slock

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ set +e
 cd ./dmenu/     && sudo make clean install && cd ..
 cd ./dwm/ && sudo make clean install && cd ..
 cd ./dwmstatus && sudo make clean install && cd ..
+cd ./lock && make && make install && cd ..
 echo "exec dwm" > $HOME/.xinitc
 echo "dwmstatus" >> /etc/rc.local
